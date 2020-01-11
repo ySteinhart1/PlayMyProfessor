54355c7364eb598b61eef6aa2e9f3ffdefe36a11
Leron <lreznikov@ucsb.edu>
2020-01-11 13:27:46 -0800

    no syntax error

--- core/src/com/mygdx/game/EventGenerator.java
+++ core/src/com/mygdx/game/EventGenerator.java
@@ -1,6 +1,9 @@
 package com.mygdx.game;
+import java.util.ArrayList;
+import com.mygdx.game.Professor.Stat;
+import com.mygdx.game.Professor.Resource;
 
 public class EventGenerator {
-    private static StatChange[] randomStatChanges;
-    private static Event[] 
+    private static ArrayList<Executable> randomStatChanges;
+    private static ArrayList<Executable> randomResourceChanges;
 }
